cdd7a3148e9fc901f305b0d7a04400e6f5e08140
shbarman <barman.s@northeastern.edu>
2020-07-03 02:31:58 -0400

    547 Friend Circle using DFS

--- Graph/friend_Circles_547.java
+++ Graph/friend_Circles_547.java
@@ -7,6 +7,7 @@ class friend_Circles_547 {
         int[][] graph = {{1,0,0,1},{0,1,1,0},{0,1,1,1},{1,0,1,1}};
         friend_Circles_547 solution = new friend_Circles_547();
         System.out.println(solution.findCircleNum(graph));
+        System.out.println(solution.dfsSolve(graph));
 
     }
     
@@ -41,6 +42,29 @@ class friend_Circles_547 {
        if(parent[i]==-1) parent[i] = find(parent,parent[i]);
 
        return parent[i];
+   }
+
+   public int dfsSolve(int[][] M){
+    int N = M.length;
+    int[] visited = new int[N];
+    int count=0;
+    for(int i=0; i<N;i++){
+        if(visited[i]==0){
+            dfs(visited,i,M);
+            count++;
+        }
+    }
+    return count;
+   }
+
+   void dfs(int[] visited, int i, int[][] M){
+       for(int j=0; j<M.length;j++){
+           if(visited[j]==0 && M[i][j]==1){
+               visited[j]=1;
+               dfs(visited,j,M);
+               
+           }
+       }
    }
 }
 
