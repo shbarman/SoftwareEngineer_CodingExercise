aa1d0ebb3cba8076ddced2d69577004dd4c9161b
shbarman <barman.s@northeastern.edu>
2020-07-04 16:37:27 -0400

    990_correction

--- Graph/satisfiabilityofEq_990.java
+++ Graph/satisfiabilityofEq_990.java
@@ -1,7 +1,7 @@
 public class satisfiabilityofEq_990{
     
     public boolean equationsPossible(String[] equations) {
-        DSU dsu = new DSU(equations.length);
+        DSU dsu = new DSU(26);
         for(String eq: equations){
             if(eq.charAt(1)=='='){
                 dsu.union(eq.charAt(0) -'a', eq.charAt(3)-'a');
@@ -10,7 +10,7 @@ public class satisfiabilityofEq_990{
 
         for(String eq:equations){
             if(eq.charAt(1)=='!'){
-               if( dsu.connected(eq.charAt(0)-'a', eq.charAt(0)-'b'))
+               if( dsu.connected(eq.charAt(0)-'a', eq.charAt(3)-'a'))
                 return false;
             }
         }
@@ -19,7 +19,7 @@ public class satisfiabilityofEq_990{
     }
 
     public static void main(String args[]){
-        String[] equations = {"a==b","b!=b"};
+        String[] equations = {"c==c","b==d","x!=z"};
         satisfiabilityofEq_990 sol = new satisfiabilityofEq_990();
         boolean result = sol.equationsPossible(equations);
         System.out.println(result);
