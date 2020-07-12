036a34775ce1fb02d130012f88c7461fec3522c6
shbarman <barman.s@northeastern.edu>
2020-07-12 01:42:13 -0400

    169

--- Arrays/majorityElem_169.java
+++ Arrays/majorityElem_169.java
@@ -1,3 +1,27 @@
+import java.util.HashMap;
+
 public class majorityElem_169 {
     
+    public static void main(String args[]){
+        majorityElem_169 sol = new majorityElem_169();
+        int nums[] = {2,2,1,1,1,2,2};
+        System.out.println(sol.majorityElement(nums));
+    }
+
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i, 0)+1);
+        }
+        int result =0;
+        int max =0;
+        for(int i:map.keySet()){
+            if(map.get(i)>nums.length/2 && map.get(i)>max ){
+                max = map.get(i);
+                result =i;
+            }
+        }
+        return result;
+    }
 }
